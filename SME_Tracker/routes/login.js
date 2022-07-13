ef19551853cc9b2e842f246d3507fc4d2b7732a1
esundbla <69823637+esundbla@users.noreply.github.com>
2022-07-13 12:30:58 -0600

    first steps

--- SME_Tracker/routes/login.js
+++ SME_Tracker/routes/login.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SME_Tracker' });
+  res.render('login');
 });
+
+router.post('/', function(req,res, next){
+    res.render('index', {title: "SME Tracker"});
+})
 
 module.exports = router;
