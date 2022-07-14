9c0fa5157ceda1a6480a8d4530cac4bfc2b30894
esundbla <69823637+esundbla@users.noreply.github.com>
2022-07-14 09:32:12 -0600

    commit

--- SME_Tracker/routes/index.js
+++ SME_Tracker/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var databaseVals = require("../db_queries")
+
+
+var val = await databaseVals({})
+console.log(val)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SME_Tracker' });
+  res.render('index', { title: val });
 });
 
 module.exports = router;
