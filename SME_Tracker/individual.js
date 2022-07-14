f11b538760385f9bd42ed836bdde0ff84506af04
esundbla <69823637+esundbla@users.noreply.github.com>
2022-07-14 17:13:27 -0600

    Queries Composed for main screen

---
+++ SME_Tracker/individual.js
@@ -0,0 +1,151 @@
+queries = [
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Develop and continously improve coaching session, webinar, and Dynatrace Unicersity content",
+    "metric": "# of sessions" },
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Develop and continously improve coaching session, webinar, and Dynatrace Unicersity content" ,
+    "metric": "# of videos"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Develop and continously improve coaching session, webinar, and Dynatrace Unicersity content" ,
+    "metric": "# of University Content" },
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Deliver Internal Technical Product Specialist training" ,
+    "metric": "# of training delivered"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Deliver Internal Technical Product Specialist training" ,
+    "metric": "%CSAT Score" },
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Partner with other teams to develop Dynatrace best practices" ,
+    "metric": "# of meetings participated in"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Partner with other teams to develop Dynatrace best practices" ,
+    "metric": "# of best practice guides"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Partner with other teams to develop Dynatrace best practices" ,
+    "metric": "# of topics developed"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Assist in building lab enviroments" ,
+    "metric": "# of lab enviroments developed"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Assist in building lab enviroments" ,
+    "metric": "# of others using labs"},
+
+    {"Overall": "Content and Lab Dev",
+    "Sub-Cat": "Assist in building lab enviroments" ,
+    "metric": "# of others participating in lab development"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Analuze chats/tickets" ,
+    "metric": "# of topics reviewed"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Analuze chats/tickets" ,
+    "metric": "# of trends identified"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Analuze chats/tickets" ,
+    "metric": "# of recommendations provided"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Develop Content to help deflect chats",
+    "metric": "# of Answer bots developed"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Develop Content to help deflect chats" ,
+    "metric": "# of TKBs created/edited"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Develop Content to help deflect chats" ,
+    "metric": "# of articles written"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Develop Content to help deflect chats" ,
+    "metric": "#/% inc. of self-help"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Develop Content to help deflect chats" ,
+    "metric": "#/%dec. in chats"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Work with Product Management/R&D to improve product self-service",
+    "metric": "# of product enhancements requested/released"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Work with Product Management/R&D to improve product self-service" ,
+    "metric": "#/% increase of self-help"},
+
+    {"Overall": "Product feedback (product, self-help, internal process)",
+    "Sub-Cat": "Work with Product Management/R&D to improve product self-service" ,
+    "metric": "#/%decrease in chats"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "Participate in cross-functional SWAT, Value Creation, etc. teams",
+    "metric": "# of meetings attended"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "Participate in cross-functional SWAT, Value Creation, etc. teams",
+    "metric": "# of groups represented in"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "Author internal, coporate, and 3rdparty blog articles",
+    "metric": "# of articles published"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat":  "Author internal, coporate, and 3rdparty blog articles",
+    "metric": "# reads per article"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "Develop and present at internal, coporate, and 3rdparty technical conferences",
+    "metric": "# of conferences attended"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "Develop and present at internal, coporate, and 3rdparty technical conferences",
+    "metric": "# of presentations shared"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "TPS work and results are recognized by global team",
+    "metric": "# of internal meetings where TPS is highlighted"},
+
+    {"Overall": "D1 represenative/evangelist (internal and external)",
+    "Sub-Cat": "TPS work and results are recognized by global team",
+    "metric": "# of meetings where TPS is presenting on a global or cross-regional basis"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Continue to expand technical knowledge and stay ahead of industry trends",
+    "metric": "# of advanced certs"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Continue to expand technical knowledge and stay ahead of industry trends",
+    "metric": "# of advanced training courses completed"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Engage on ad-hoc customer meetings/coaching sessions/Q&A",
+    "metric": "# of customer meetings on SME topic, CSAT%"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Act as an escalation point",
+    "metric": "# of hours in chat outside of shift"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Act as an escalation point",
+    "metric": "# of escalations handled"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Participate in internal process development and continous improvement",
+    "metric": "# of new/improved processes"},
+
+    {"Overall": "Recognition & impact",
+    "Sub-Cat": "Participate in internal process development and continous improvement",
+    "metric": "% time/efficiency improvement"}
+]
+
+module.exports = queries
--- SME_Tracker/routes/index.js
+++ SME_Tracker/routes/index.js
@@ -1,12 +1,30 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 var databaseVals = require("../db_queries")
-
+var user = "Erik.Sundblad"
+var queries = require('../individual.js')
+results =[]
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  val = await databaseVals({})
-  res.render('index', { title: val });
+  for (let query in queries) {
+    console.log(queries[query])
+    val = await databaseVals(queries[query])
+    console.log(val)
+    results.push(val)
+  }
+  res.render('index', { a1: results[0],
+                        a2: results[1],
+                        a3: results[2],
+                        a4: results[3],
+                        a5: results[4],
+                        a6: results[5],
+                        a7: results[6],
+                        a8: results[7],
+                        a9: results[8],
+                        a10: results[9],
+                      });    
 });
 
 module.exports = router;
